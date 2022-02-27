9abb45866413f9bd0d7b1925b950901c3f4e03e9
mzztrue <inseongold@gmail.com>
2022-02-28 00:28:23 +0800

    network

---
+++ network.py
@@ -0,0 +1,396 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  9 11:42:58 2021
+
+@author: swis
+"""
+import torch
+import torch.nn as nn
+from mmd_pytorch import MMD_loss
+
+def load_pretrained_net(model,path):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    pretrained_dict = torch.load(path,map_location = device)
+    model_dict = model.state_dict()
+
+    keys = []
+    for k, v in pretrained_dict.items():
+        keys.append(k)
+    i = 0
+    for k, v in model_dict.items():
+        if v.size() == pretrained_dict[keys[i]].size():
+            model_dict[k] = pretrained_dict[keys[i]]
+            i = i + 1
+    model.load_state_dict(model_dict)
+
+
+class DA_Alex(nn.Module):
+    def __init__(self, num_classes=5):
+        super(DA_Alex, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True)
+        )
+        self.bottleneck = nn.Sequential(
+            nn.Linear(4096, 256),
+            nn.ReLU(inplace=True)
+        )
+        self.mmd = MMD_loss(kernel_type='rbf')
+        self.final_classifier = nn.Sequential(
+            nn.Linear(256, num_classes)
+        )
+        
+    def forward(self, source, target):
+        source = self.features(source)
+        source = source.view(source.size(0), -1)
+        source = self.classifier(source)
+        source = self.bottleneck(source)
+        mmdloss = 0.
+        if self.training:
+            target = self.features(target)
+            target = target.view(target.size(0), -1)
+            target = self.classifier(target)
+            target = self.bottleneck(target)
+            mmdloss += self.mmd(source, target)
+        result = self.final_classifier(source)
+
+        return result, mmdloss
+
+class DA_Alex_FC1(nn.Module):
+    def __init__(self, num_classes=5):
+        super(DA_Alex_FC1, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+
+        )
+        self.mmd = MMD_loss(kernel_type='rbf')
+        self.bottleneck = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, 256),
+            nn.ReLU(inplace=True),
+        )
+        self.final_classifier = nn.Sequential( 
+            nn.Linear(256, num_classes)
+        )
+        
+    def forward(self, source, target):
+        source = self.features(source)
+        source = source.view(source.size(0), -1)
+        source = self.classifier(source)
+        source = self.bottleneck(source)
+        mmdloss = 0.
+        if self.training:
+            target = self.features(target)
+            target = target.view(target.size(0), -1)
+            target = self.classifier(target)
+            target = self.bottleneck(target)
+            mmdloss += self.mmd(source, target)
+        result = self.final_classifier(source)
+
+        return result, mmdloss
+    
+class Alexnet_finetune(nn.Module):
+    def __init__(self, num_classes=5):
+        super(Alexnet_finetune, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True)
+        )
+        self.final_classifier = nn.Sequential(
+            nn.Linear(4096, num_classes)
+        )
+
+    def forward(self, input):
+        x = self.features(input)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        output = self.final_classifier(x)
+        return output
+
+
+class LeNet_finetune(nn.Module):
+    def __init__(self, num_classes=5):
+        super(LeNet_finetune, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2,2)            
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(16*53*53, 120),
+            nn.ReLU(inplace=True),
+            nn.Linear(120,84),
+            nn.ReLU(inplace=True)     
+        )
+
+        self.final_classifier = nn.Sequential(
+            nn.Linear(84,num_classes)
+        )
+
+    def forward(self, source):
+        x = self.features(source)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        result = self.final_classifier(x)
+        return result
+
+class DA_LeNet(nn.Module):
+    def __init__(self, num_classes=5):
+        super(DA_LeNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2,2)            
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(16*53*53, 120),
+            nn.ReLU(inplace=True),
+            nn.Linear(120,84),
+            nn.ReLU(inplace=True)     
+        )
+        self.mmd = MMD_loss(kernel_type='rbf')
+        self.final_classifier = nn.Sequential(
+            nn.Linear(84,num_classes)
+        )
+
+    def forward(self, source, target):
+        x = self.features(source)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        mmdloss = 0.
+        if self.training:
+            y = self.features(target)
+            y = y.view(y.size(0), -1)
+            y = self.classifier(y)
+            mmdloss += self.mmd(x, y)
+        result = self.final_classifier(x)
+        return result, mmdloss
+
+class DA_LeNet_FC1(nn.Module):
+    def __init__(self, num_classes=5):
+        super(DA_LeNet_FC1, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2,2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(16*53*53, 120),
+            nn.ReLU(inplace=True),   
+        )
+        self.mmd = MMD_loss(kernel_type='rbf')
+        self.final_classifier = nn.Sequential( 
+            nn.Linear(120,84),
+            nn.ReLU(inplace=True),
+            nn.Linear(84,num_classes)
+        )
+
+    def forward(self, source, target):
+        x = self.features(source)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        mmdloss = 0.
+        if self.training:
+            y = self.features(target)
+            y = y.view(y.size(0), -1)
+            y = self.classifier(y)
+            mmdloss += self.mmd(x, y)
+        result = self.final_classifier(x)
+        return result, mmdloss
+
+class DA_LeNet_FC2(nn.Module):
+    def __init__(self, num_classes=5):
+        super(DA_LeNet_FC2, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2,2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(16*53*53, 120),
+            nn.ReLU(inplace=True),   
+            nn.Linear(120,84),
+            nn.ReLU(inplace=True)
+        )
+        self.mmd = MMD_loss(kernel_type='rbf')
+        self.final_classifier = nn.Sequential( 
+            nn.Linear(84,num_classes)
+        )
+
+    def forward(self, source, target):
+        x = self.features(source)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        mmdloss = 0.
+        if self.training:
+            y = self.features(target)
+            y = y.view(y.size(0), -1)
+            y = self.classifier(y)
+            mmdloss += self.mmd(x, y)
+        result = self.final_classifier(x)
+        return result, mmdloss
+
+class VGG_finetune(nn.Module):
+    def __init__(self,num_classes=5,batch_norm = True):
+        super(VGG_finetune, self).__init__()
+        cfg = {
+    'A': [64,     'M', 128,      'M', 256, 256,           'M', 512, 512,           'M', 512, 512,           'M'],
+    'B': [64, 64, 'M', 128, 128, 'M', 256, 256,           'M', 512, 512,           'M', 512, 512,           'M'],
+    'C': [64, 64, 'M', 128, 128, 'M', 256, 256, 256,      'M', 512, 512, 512,      'M', 512, 512, 512,      'M'],
+    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+    }
+        layers = []
+        input_channel = 3
+        for l in cfg['D']:
+            if l == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+                continue
+            layers += [nn.Conv2d(input_channel, l, kernel_size=3, padding=1)] # stride默认为1,即保持图像尺寸不变
+            if batch_norm == True:
+                layers += [nn.BatchNorm2d(l)]
+            layers += [nn.ReLU(inplace=True)]
+            input_channel = l
+
+
+        self.features = nn.Sequential(*layers)
+        self.classifier = nn.Sequential(
+            nn.Linear(7 * 7 * 512, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            
+            )
+        self.final_classifier = nn.Sequential(
+            nn.Linear(4096, num_classes)
+            )
+        
+    def forward(self,input):
+        x = self.features(input)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        output = self.final_classifier(x)
+
+        return output
+    
+class DA_VGG(nn.Module):
+    def __init__(self,num_classes=5,batch_norm = True):
+        super(DA_VGG, self).__init__()
+        cfg = {
+    'A': [64,     'M', 128,      'M', 256, 256,           'M', 512, 512,           'M', 512, 512,           'M'],
+    'B': [64, 64, 'M', 128, 128, 'M', 256, 256,           'M', 512, 512,           'M', 512, 512,           'M'],
+    'C': [64, 64, 'M', 128, 128, 'M', 256, 256, 256,      'M', 512, 512, 512,      'M', 512, 512, 512,      'M'],
+    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+    }
+        layers = []
+        input_channel = 3
+        for l in cfg['D']:
+            if l == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+                continue
+            layers += [nn.Conv2d(input_channel, l, kernel_size=3, padding=1)] # stride默认为1,即保持图像尺寸不变
+            if batch_norm == True:
+                layers += [nn.BatchNorm2d(l)]
+            layers += [nn.ReLU(inplace=True)]
+            input_channel = l
+
+
+        self.features = nn.Sequential(*layers)
+        self.classifier = nn.Sequential(
+            nn.Linear(7 * 7 * 512, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            )
+        self.mmd = MMD_loss(kernel_type='rbf')
+        self.final_classifier = nn.Sequential(
+            nn.Linear(4096, num_classes)
+            )
+    
+    def forward(self, source, target):
+        x = self.features(source)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        mmdloss = 0.
+        if self.training:
+            y = self.features(target)
+            y = y.view(y.size(0), -1)
+            y = self.classifier(y)
+            mmdloss += self.mmd(x, y)
+        result = self.final_classifier(x)
+        return result, mmdloss
---
+++ train_test_save.py
@@ -0,0 +1,115 @@
+import torch
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import shutil
+
+from tools import get_uar
+
+
+def train(device, source_loader, model, criterion, optimizer, epoch):
+    
+    corrects = 0
+    total = 0
+    total_loss = 0.
+
+    model.train()
+    for source_input in source_loader:
+        data, labels = source_input
+        data,labels = data.to(device), labels.to(device)
+
+        preds = model(data)  
+        print(preds,labels)      
+        loss = criterion(preds, labels)
+        corrects += preds.argmax(dim=1).eq(labels).sum().item()
+        total += len(labels)
+        total_loss += loss.item()*len(labels) 
+
+        optimizer.zero_grad()
+        loss.backward()     
+        optimizer.step()
+
+    print("train:",preds.argmax(dim=1), labels)
+    acc = corrects/total
+    lss = total_loss / total
+
+    return acc,lss
+    
+def test(device, target_loader, model,da=1):
+    corrects = 0
+    total = 0
+    
+    all_preds = torch.tensor([]).long()
+    all_labels = torch.tensor([]).long()
+
+    model.eval()
+    with torch.no_grad():
+        for target_input in target_loader:
+            data, labels = target_input
+            data, labels = data.to(device), labels.to(device)
+            if(da):
+                preds, _ = model(data, data)
+            else:
+                preds = model(data)#.long()
+
+            corrects += preds.argmax(dim=1).eq(labels).sum().item()
+            total += len(labels)
+            all_labels = torch.cat((all_labels, labels.cpu()),dim=0)
+            all_preds = torch.cat((all_preds, preds.cpu()),dim=0)
+       
+    acc = corrects/total
+    # print("test:",all_preds.argmax(dim=1), all_labels)
+    cm = confusion_matrix((all_labels), all_preds.argmax(dim=1),normalize=None)
+    uar = get_uar(cm)
+    return acc,uar,cm
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'model_best.pth.tar')
+        print("best saved")        
+
+def dadcnn_train(device,source_loader, target_loader, model, criterion, optimizer, epoch, alpha):
+    
+    model.train()
+    
+    iter_source = iter(source_loader)
+    iter_target = iter(target_loader)
+    num_iter = len(source_loader)
+    
+    corrects = 0
+    total = 0
+    total_loss = 0.
+    total_clf_lss = 0.
+    total_mmd_lss = 0.
+    
+    for i in range(1, num_iter+1):
+        source_data, source_label = iter_source.next()
+        target_data, _ = iter_target.next()
+        if i % len(target_loader) == 0: 
+            iter_target = iter(target_loader)
+        source_data, source_label = source_data.to(device), source_label.to(device)
+        target_data = target_data.to(device)
+        
+        source_preds, mmd_lss = model(source_data, target_data) 
+        clf_lss = criterion(source_preds, source_label)
+        
+        loss = (clf_lss +  alpha * mmd_lss)
+
+        corrects += source_preds.argmax(dim=1).eq(source_label).sum().item()
+        total += len(source_label)
+        
+        total_loss += loss.item()*len(source_label)
+        total_clf_lss += clf_lss.item()*len(source_label)
+        total_mmd_lss += mmd_lss.item()*len(source_label)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+      
+
+    acc = corrects/total
+    lss = total_loss / total
+    mean_clf_lss = total_clf_lss/total
+    mean_mmd_lss = total_mmd_lss/total
+
+    return acc, lss, mean_clf_lss, mean_mmd_lss
