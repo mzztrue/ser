f6b7f36f2a0f142619b607e2825f6a8882beac50
mzztrue <inseongold@gmail.com>
2022-02-27 23:39:03 +0800

    test spectrum transformation

--- audioset.py
+++ audioset.py
@@ -4,81 +4,80 @@ Created on Thu Jul  8 19:35:33 2021
 
 @author: swis
 """
-import os
 
+import os
 import torch
 import torchaudio
+import torchvision.transforms as T
+import torchvision.transforms.functional as F
 
 from torch.utils.data import Dataset
 
 import tools
-from tools import rechannel,resample,pad_trunc,plot_spectrogram,plot_waveform
-
-#labels to emotions
-enter_dict = {"an":1, "fe":2, "ha":3, "sa":5, "su":6, "di":7}
-emodb_dict = {"W":1, "A":2, "F":3, "N":4, "T":5, "E":7, "L":8}
-en_em_list = [1,2,3,5,7]
-ca_en_list = [1,2,3,5,6]
-ca_em_list = [1,2,3,5]
-
-  #-------------------------------------------------------------------
-  #In the common case that you use CrossEntropyLoss 7 as your loss
-  #function, per its documentation, your labels (the target) must be
-  #integer class labels that run from [0, nClass - 1], inclusive. That
-  #is, the labels start from 0.
-  #------------------------------------------------------------------------
-def label_2_cross_entrophy_class(emo,label_list):
-    for idx, label in enumerate (label_list):
-        if emo == label:
-            return idx 
-            
-
 
 class Audioset(Dataset):
-  def __init__(self, root, name_text, relative_aud_dir , labeltype):
+    '''build the audio dataset to retrieve audio samples'''
 
-    self.aud_dir_prefix = os.path.join(root,relative_aud_dir)
-    self.labeltype = labeltype
+    def __init__(self, root, name_text, relative_aud_dir, labeltype):
 
-    self.duration = 3
-    self.sr = 16000
-    self.channel = 1
-    
-    self.aud_names = []
-    self.aud_labels = []
+        self.aud_dir_prefix = os.path.join(root, relative_aud_dir)
+        self.labeltype = labeltype
 
-    namefile = os.path.join(root,name_text)
-    with open(namefile, 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            x,y =line.strip().split(" ")
-            self.aud_names.append(x)
-            self.aud_labels.append(y)
+      # -------------------------------------------------------
+      # standard audio sample: dur = 3s, sr = 16k, one channel
+      # -------------------------------------------------------
+        self.duration = 3
+        self.sample_rate = 16000
+        self.channel = 1
+        self.aud_names = []
+        self.aud_labels = []
 
-  # ----------------------------
-  # Number of items in dataset
-  # ----------------------------
-  def __len__(self):
-    return len(self.aud_labels)    
-    
-  # ----------------------------
-  # Get i'th item in dataset
-  # ----------------------------
-  def __getitem__(self, idx):
+        namefile = os.path.join(root, name_text)
+        with open(namefile, 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                x,y = line.strip().split(" ")
+                self.aud_names.append(x)
+                self.aud_labels.append(y)
 
-    # Absolute file path of the ith audio file - concatenate aud_dir with aud_names[i]
-    aud_dir = os.path.join(self.aud_dir_prefix, self.aud_names[idx])
+    # ----------------------------
+    # Number of items in dataset
+    # ----------------------------
+    def __len__(self):
+        return len(self.aud_labels)
 
-    # Get the Class ID
-    emo = int(self.aud_labels[idx])
-    emo = label_2_cross_entrophy_class(emo,self.labeltype)
-    label = torch.tensor(emo, dtype = torch.long)
-    
-    aud  = torchaudio.load(aud_dir)
-    waveform, sr = aud[0],aud[1]
-    resam = resample(waveform, self.sr)
-    rechan = rechannel(resam, self.channel)
-    aud = pad_trunc(rechan, self.sr,self.duration)
-    mel_spec=tools.mel_spectrogram(aud)
+    # ----------------------------
+    # Get i'th item in dataset
+    # ----------------------------
+    def __getitem__(self, idx):
 
-    return mel_spec, label
+        # Absolute file path of the ith audio file - concatenate aud_dir with aud_names[i]
+        aud_dir = os.path.join(self.aud_dir_prefix, self.aud_names[idx])
+
+        # Get the Class ID
+        emo = int(self.aud_labels[idx])
+
+        def label_2_cross_entrophy_class(emo, label_list):
+            for idx, label in enumerate(label_list):
+                if emo == label:
+                    return idx
+
+        emo = label_2_cross_entrophy_class(emo, self.labeltype)
+        label = torch.tensor(emo, dtype = torch.long)
+
+        aud = torchaudio.load(aud_dir)
+        waveform, old_sample_rate = aud[0], aud[1]
+        waveform = tools.resample(waveform, old_sample_rate, self.sample_rate)
+        waveform = tools.rechannel(waveform, self.channel)
+        waveform = tools.pad_trunc(waveform, self.sample_rate, self.duration)
+
+        mel_spec = tools.mel_spectrogram(waveform)
+
+        #do random crop and flip to mel spectrum image, as data augmentation
+        preprocess = T.Compose([
+            T.RandomCrop((224, 224)),
+            T.RandomHorizontalFlip(),
+        ])
+        resized_mel_spec = preprocess(F.resize(mel_spec, (256, 256))).repeat(3, 1, 1)
+
+        return resized_mel_spec, label
--- tools.py
+++ tools.py
@@ -1,72 +1,55 @@
 import random
 import librosa
+import numpy as np
+import itertools
 
 import torch
 import torchaudio
 import matplotlib.pyplot as plt
 
-#plot tools
-def plot_waveform(waveform, sample_rate, title="Waveform", xlim=None, ylim=None):
-  waveform = waveform.numpy()
+#-----------------------------------------------------
+# turn into standard sample(default: 1 channel, 16k sr, 3 sec)
+#-----------------------------------------------------
 
-  num_channels, num_frames = waveform.shape
-  time_axis = torch.arange(0, num_frames) / sample_rate
-
-  figure, axes = plt.subplots(num_channels, 1)
-  if num_channels == 1:
-    axes = [axes]
-  for c in range(num_channels):
-    axes[c].plot(time_axis, waveform[c], linewidth=1)
-    axes[c].grid(True)
-    if num_channels > 1:
-      axes[c].set_ylabel(f'Channel {c+1}')
-    if xlim:
-      axes[c].set_xlim(xlim)
-    if ylim:
-      axes[c].set_ylim(ylim)
-  figure.suptitle(title)
-  plt.show(block=False)
-    
-def plot_spectrogram(spec, title=None, ylabel='freq_bin', aspect='auto', xmax=None):
-  fig, axs = plt.subplots(1, 1)
-  axs.set_title(title or 'Spectrogram (db)')
-  axs.set_ylabel(ylabel)
-  axs.set_xlabel('frame')
-  im = axs.imshow(librosa.power_to_db(spec), origin='lower', aspect=aspect)
-  if xmax:
-    axs.set_xlim((0, xmax))
-  fig.colorbar(im, ax=axs)
-  plt.show(block=False)
-
-#set to same channel
 def rechannel(waveform, new_channel=1):
-
+    """
+      take in waveform vector(channel, data) and new channel numbers,
+      return rechanneled waveform tuple
+    """
     if (waveform.shape[0] == new_channel):
-      return waveform
+        return waveform
 
     if (new_channel == 1):
-      # Convert from stereo to mono by selecting only the first channel
-      new_waveform = waveform[:1, :]
-      return new_waveform
-    
-#standardize sample rate
-def resample(waveform, sr, newsr=16000):
-  if (sr == newsr):
-    return waveform
+        # Convert from stereo to mono by selecting only the first channel
+        new_waveform = waveform[:1, :]
+        return new_waveform
+
+def resample(waveform, oldsr, newsr=16000):
+  """
+  take in waveform vector, oldsr, newsr,
+  return resampled waveform tuple
+  """
+  if (oldsr == newsr):
+      return waveform
   # Resample first channel
-  new_waveform = torchaudio.transforms.Resample(sr, newsr)(waveform[:1,:])
+  new_waveform = torchaudio.transforms.Resample(oldsr, newsr)(waveform[:1, :])
   return new_waveform
 
-#standardize length
-def pad_trunc(wf, sr, max_s):
-    num_rows, wf_len = wf.shape    
-    max_len = sr * max_s
+def pad_trunc(wf, sr, max_s=3):
+  """
+  take waveform vector, sample rate * seconds as data point numbers,
+  pad or truncate length of waveform vector
+  """
+  num_rows, wf_len = wf.shape
+  max_len = sr * max_s
 
-    if (wf_len > max_len):
+  if (wf_len > max_len):
       # Truncate the signal to the given length
-      wf = wf[:,:max_len]
+      begin_idx = random.randint(0, wf_len - max_len)
+      end_idx = begin_idx + max_len
+      wf = wf[:, begin_idx:end_idx]
 
-    elif (wf_len < max_len):
+  elif (wf_len < max_len):
       # Length of padding to add at the beginning and end of the signal
       pad_begin_len = random.randint(0, max_len - wf_len)
       pad_end_len = max_len - wf_len - pad_begin_len
@@ -75,38 +58,41 @@ def pad_trunc(wf, sr, max_s):
       pad_begin = torch.zeros((num_rows, pad_begin_len))
       pad_end = torch.zeros((num_rows, pad_end_len))
       wf = torch.cat((pad_begin, wf, pad_end), 1)
-    return wf
+  return wf
 
-#----------------------------------------------
-#1.make frames
-# Typical frame sizes in speech processing range from 20 ms to 40 ms with 50% (+/-10%) overlap between consecutive frames. 
-# Popular settings are 25 ms for the frame size, 
-#
-# frame_size = 0.025 and a 10 ms stride (15 ms overlap), frame_stride = 0.001.
-#wind_length = 0.025*16000=400,hop_length = 0.01*16000=160
-#
-#2.hamming window
-#3.nfft
-#We can now do an N-point FFT on each frame to calculate the frequency spectrum, 
-# which is also called Short-Time Fourier-Transform (STFT), where N is typically 256 or 512, NFFT = 512; 
-# and then compute the power spectrum (periodogram) 
-#4.mel filter bank
-# The final step to computing filter banks is applying triangular filters, 
-# typically 40 filters, nfilt = 40 on a Mel-scale to the power spectrum to extract frequency bands. 
-# The Mel-scale aims to mimic the non-linear human ear perception of sound, 
-# by being more discriminative at lower frequencies and less discriminative at higher frequencies. 
-#
-#
-#
-n_fft = 1024
+
+# ----------------------------------------------
+# 1.make frames
+# Typical frame sizes in speech processing range from 20 ms to 40 ms 
+# with 50% (+/-10%) overlap between consecutive frames.
+# Popular settings are 25 ms for the frame size, and a 10 ms stride (15 ms overlap),
+# frame_size = 0.025  frame_stride = 0.001.
+# wind_length = 0.025*16000=400, hop_length = 0.01*16000=160
+
+# 2.hamming window
+# add hamming window filter
+
+# 3.nfft
+# do an N-point FFT on each frame to calculate the frequency spectrum,and then compute the power spectrum (periodogram)
+# where N is typically 256 or 512, 
+# NFFT = 512
+
+# 4.mel filter bank
+# The final step to computing filter banks is applying triangular filters,
+# typically 40 filters, nfilt = 40 on a Mel-scale to the power spectrum to extract frequency bands.
+# The Mel-scale aims to mimic the non-linear human ear perception of sound,
+# by being more discriminative at lower frequencies and less discriminative at higher frequencies.
+
+
+  # n_fft = 1024
+  # win_length = n_fft
+  # hop_length = 512
+  # n_mels = 128
+
+n_fft = 512
 win_length = n_fft
-hop_length = 512
-n_mels = 128
-
-# n_fft = 512
-# win_length = n_fft
-# hop_length = 256
-# n_mels = 40
+hop_length = 256
+n_mels = 40
 
 mel_spectrogram = torchaudio.transforms.MelSpectrogram(
     sample_rate=16000,
@@ -121,3 +107,141 @@ mel_spectrogram = torchaudio.transforms.MelSpectrogram(
     n_mels=n_mels,
     mel_scale="htk",
 )
+
+#---------------------
+# plot tools
+#---------------------
+def plot_waveform(waveform, sample_rate, title="Waveform", xlim=None, ylim=None):
+
+    waveform = waveform.numpy()
+    num_channels, num_frames = waveform.shape
+    time_axis = torch.arange(0, num_frames) / sample_rate
+
+    figure, axes = plt.subplots(num_channels, 1) #return squeezed axes, in this case, num of channels
+
+    if num_channels == 1:
+        axes = [axes] #unsqueeze axes
+
+    for c in range(num_channels):
+        axes[c].plot(time_axis, waveform[c], linewidth=1)
+        axes[c].grid(True)
+        if num_channels > 1:
+            axes[c].set_ylabel(f'Channel {c+1}') #f-string format
+        if xlim:
+            axes[c].set_xlim(xlim)
+        if ylim:
+            axes[c].set_ylim(ylim)
+
+    figure.suptitle(title)
+    plt.show(block=False)
+
+def plot_spectrogram(spec, title=None, ylabel='freq_bin', aspect='auto', xmax=None):
+    fig, axs = plt.subplots(1, 1)
+    axs.set_title(title or 'Spectrogram (db)')
+    axs.set_ylabel(ylabel)
+    axs.set_xlabel('frame')
+    im = axs.imshow(librosa.power_to_db(spec), origin='lower', aspect=aspect)
+    if xmax:
+        axs.set_xlim((0, xmax))
+    fig.colorbar(im, ax=axs)
+    plt.show(block=False)
+
+def plot_confusion_matrix(cm, class_names, normalize=False, title='Confusion matrix', figsize=[6.4, 4.8], cmap=plt.cm.Blues):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    figure = plt.figure(figsize=figsize)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(class_names))
+    plt.xticks(tick_marks, class_names, rotation=45)
+    plt.yticks(tick_marks, class_names)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    return figure
+
+
+def get_all_labels_preds(model, loader):
+    all_preds = torch.tensor([])
+    all_labels = torch.tensor([])
+    for batch in loader:
+        images, labels = batch
+        preds = model(images)
+        all_labels = torch.cat((all_labels, labels), dim=0)
+        all_preds = torch.cat((all_preds, preds), dim=0)
+    return all_labels, all_preds
+
+
+def get_uar(cm):
+    tp = []
+    fn = []
+    uar = []
+    for i in range(cm.shape[0]):
+        false = 0
+        for j in range(cm.shape[1]):
+            if i == j:
+                tp.append(cm[i][j])
+            else:
+                false += cm[i][j]
+        fn.append(false)
+        uar.append(tp[-1]/(tp[-1]+fn[-1]))
+    uar = sum(uar)/len(uar)
+    return uar
+
+
+def add_confusion_matrix(
+    writer,
+    cmtx,
+    num_classes,
+    global_step=None,
+    subset_ids=None,
+    class_names=None,
+    tag="Confusion Matrix",
+    figsize=None,
+):
+    """
+    Calculate and plot confusion matrix to a SummaryWriter.
+    Args:
+        writer (SummaryWriter): the SummaryWriter to write the matrix to.
+        cmtx (ndarray): confusion matrix.
+        num_classes (int): total number of class_names.
+        global_step (Optional[int]): current step.
+        subset_ids (list of ints): a list of label indices to keep.
+        class_names (list of strs, optional): a list of all class names.
+        tag (str or list of strs): name(s) of the confusion matrix image.
+        figsize (Optional[float, float]): the figure size of the confusion matrix.
+            If None, default to [6.4, 4.8].
+
+    """
+    if subset_ids is None or len(subset_ids) != 0:
+        # If class names are not provided, use class indices as class names.
+        if class_names is None:
+            class_names = [str(i) for i in range(num_classes)]
+        # If subset is not provided, take every class_names.
+        if subset_ids is None:
+            subset_ids = list(range(num_classes))
+
+        sub_cmtx = cmtx[subset_ids, :][:, subset_ids]
+        sub_names = [class_names[j] for j in subset_ids]
+
+        sub_cmtx = plot_confusion_matrix(
+            sub_cmtx,
+            class_names=sub_names,
+            normalize=False,
+            title=tag,
+            figsize=figsize
+        )
+        # Add the confusion matrix image to writer.
+        writer.add_figure(tag=tag, figure=sub_cmtx, global_step=global_step)
